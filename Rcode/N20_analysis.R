a3cd80138f439e7d50f56174af7cdb3c44731b75
Johnny Soares <j159194@dac.unicamp.br>
2017-02-24 10:36:53 -0300

    update in analysis

--- Rcode/N20_analysis.R
+++ Rcode/N20_analysis.R
@@ -4,7 +4,7 @@ setwd("C:/Users/johnny.soares/Desktop/testeR")
 N2O2014<-read.csv("C:/Users/johnny.soares/Desktop/testeR/data/N2O2014.csv")
 
 #Reading file based on relative pah (setwd is important)
-N2O2014<-read.csv("testeR/data/N2O2014.csv")
+N2O2014<-read.csv("./data/N2O2014.csv")
 
 # Historgam of N20 on log scale
 png("Output/histogram_N20.png")  # to open the file 
@@ -24,3 +24,34 @@ dev.off()
 
 #Normal distribution
 shapiro.test(resid(anova))
+
+#Histogram of residuals of anova
+hist(x=resid(anova))
+
+#read N2O means
+N2Omean<-read.csv("./data/N2O2014Media.csv")
+
+#plot N2Omeans
+#first read package
+library(ggplot2)
+
+plot_of_N20 <- ggplot(data=N2Omean, aes(x= as.factor(Fonte), y= N2O))
+plot_of_N20 <- plot_of_N20 + geom_bar(stat="identity", aes(fill="factor(Fonte)"))
+plot(plot_of_N20)
+
+#plot box plot
+plot(N2O2014, x=N2O2014$Fonte, y=N2O2014$N2Olog)
+
+# box plot in ggplot2
+boxplot_of_N20 <- ggplot(data=N2O2014, aes(x= as.factor(N2O2014$Fonte), y= N2O2014$N2Olog))
+boxplot_of_N20 + geom_boxplot()
+#adding replicates values
+boxplot_of_N20 + geom_boxplot() + geom_jitter()
+#putting color
+boxplot_of_N20 + geom_boxplot(aes(fill = N2O2014$Fonte)) + geom_jitter()
+#changing color
+boxplot_of_N20 + geom_boxplot(aes(fill = N2O2014$Fonte)) + geom_jitter() + scale_fill_grey() + theme_classic()
+# editing axis, title
+boxplot_of_N20 + geom_boxplot() + geom_jitter() +  xlab("N source") + ylab("Cumulative N2O-N Emission (log g/ha)")
+
+
